764eb93bad1007ee84c5735c1fb034e12802645a
Liaess <yann.melo@gmail.com>
2021-12-21 10:47:21 -0300

    Preparando revisão de código

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CheckOut } from './CheckOut'; 
-export { default as MovieList } from './MovieList'; 
-export { default as Seats } from './Seats'; 
-export { default as Sessions } from './Sessions';
